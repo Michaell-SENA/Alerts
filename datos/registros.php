14e8e14c06a05de57a3f1d197b936c47b48ebc93
Michaell-SENA <85643075+Michaell-SENA@users.noreply.github.com>
2021-06-16 22:25:09 -0500

    Documentación y BD actualizada

--- datos/registros.php
+++ datos/registros.php
@@ -5,21 +5,26 @@
 
 	require_once("conexion.php");
 
+
+    //captura el nombre de administrador enviado por url, luego esguardado por una bariable para hacer la validacion de los datos sql.
 	if (isset($_GET['nombre'])) 
 	{
 		
 		$nombreS = $_GET['nombre'];
 
-		//Tratada de datos para mostrar los resultados en una tabla HTML.
+		
 		$query = "SELECT t1.nombre, t1.apellido, t2.nombre_doc, t1.num_doc, t1.telefono, t1.direccion, t5.nombre_nivel, t1.programa, t1.ficha, t3.nombre_jornada, t7.nombre_sede, t4.nombre_mot_reporte, t6.nombre_reporte, t1.accion, t1.documento_soporte, t1.nombre_ins, t1.apellido_ins, t1.telefono_ins, t1.correo_ins FROM obj_alerta AS t1, obj_doc_sena AS t2, obj_jornada_sena AS t3, obj_mot_reporte_sena AS t4, obj_nivel_forma_sena AS t5, obj_per_reporte_sena AS t6, obj_sede_sena AS  t7 WHERE t1.responsable = '$nombreS' AND t2.id_obj_doc_sena = t1.doc AND t5.id_obj_nivel_sena = t1.nivel AND t1.jornada = t3.id_obj_jornada_sena AND t1.sede = t7.id_obj_sede_sena AND t1.cusa_reporte = t4.id_obj_mot_reporte_sena AND t1.reporte_diri = t6.id_obj_per_reporte_sena;";
 
+        //se encarga de recoger los datos enviados des de el buscador.
 		if(isset($_POST['busqueda']))
     	{
 
+            //se encarga de validar los datos enviados des de el buscador.
 	        if(!empty($_POST['busqueda']))
 	        {
 
-	            $busquedaa = explode(" ",$_POST['busqueda']);
+	           //se encarga de dividir el string para permitir que el sistema identifique por partes la info enviada.
+                $busquedaa = explode(" ",$_POST['busqueda']);
 
 	            $query = "SELECT t1.nombre, t1.apellido, t2.nombre_doc, t1.num_doc, t1.telefono, t1.direccion, t5.nombre_nivel, t1.programa, t1.ficha, t3.nombre_jornada, t7.nombre_sede, t4.nombre_mot_reporte, t6.nombre_reporte, t1.accion, t1.documento_soporte, t1.nombre_ins, t1.apellido_ins, t1.telefono_ins, t1.correo_ins FROM obj_alerta AS t1, obj_doc_sena AS t2, obj_jornada_sena AS t3, obj_mot_reporte_sena AS t4, obj_nivel_forma_sena AS t5, obj_per_reporte_sena AS t6, obj_sede_sena AS  t7 WHERE t1.responsable = '$nombreS' AND t2.id_obj_doc_sena = t1.doc AND t5.id_obj_nivel_sena = t1.nivel AND t1.jornada = t3.id_obj_jornada_sena AND t1.sede = t7.id_obj_sede_sena AND t1.cusa_reporte = t4.id_obj_mot_reporte_sena AND t1.reporte_diri = t6.id_obj_per_reporte_sena AND t1.num_doc LIKE '%$busquedaa[0]%'";
 
